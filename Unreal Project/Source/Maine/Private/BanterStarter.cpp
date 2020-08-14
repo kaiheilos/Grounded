c05dc92e2c1c84aad1e669448530020d71956ba0
kaiheilos <kaiheilos@gmail.com>
2020-08-14 19:52:14 +0100

    More components

--- Unreal Project/Source/Maine/Private/BanterStarter.cpp
+++ Unreal Project/Source/Maine/Private/BanterStarter.cpp
@@ -1,22 +1,21 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "HealthComponent.h"
+#include "BanterStarter.h"
 
 // Sets default values for this component's properties
-UHealthComponent::UHealthComponent()
+UBanterStarter::UBanterStarter()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-	CurrentDamage = 0.0f;
-	MaxHealth = 0.0f;
+
 	// ...
 }
 
 
 // Called when the game starts
-void UHealthComponent::BeginPlay()
+void UBanterStarter::BeginPlay()
 {
 	Super::BeginPlay();
 
@@ -26,11 +25,10 @@ void UHealthComponent::BeginPlay()
 
 
 // Called every frame
-void UHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UBanterStarter::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
 }
 
-float UHealthComponent::GetMaxHealth() { return 0.0f; }
---
+++ Unreal Project/Source/Maine/Private/SurvivalCharacter.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SurvivalCharacter.h"
+
+// Sets default values
+ASurvivalCharacter::ASurvivalCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	AttackHandlerComponent = CreateDefaultSubobject<UAttackHandlerComponent>("AttackHandlerComponent");
+	AttractionComponent = CreateDefaultSubobject<UAttractionComponent>("AttractionComponent");
+	BlockComponent = CreateDefaultSubobject<UBlockComponent>("BlockComponent");
+	HealthComponent = CreateDefaultSubobject<UHealthComponent>("HealthComponent");
+	//	CharMoveComp = CreateDefaultSubobject<UMaineCharMovementComponent>("CharMoveComp");
+	ReactionComponent = CreateDefaultSubobject<UReactionComponent>("ReactionComponent");
+	StatusEffectComponent = CreateDefaultSubobject<UStatusEffectComponent>("StatusEffectComponent");
+	TeamComponent = CreateDefaultSubobject<UTeamComponent>("TeamComponent");
+	ObsidianIDComponent = CreateDefaultSubobject<UObsidianIDComponent>("ObsidianIDComponent");
+}
+
+// Called when the game starts or when spawned
+void ASurvivalCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASurvivalCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ASurvivalCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
